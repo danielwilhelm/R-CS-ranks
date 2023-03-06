b440cd5d14758762d314e6f4846a89bebba7d3b6
MrDomani <pawel.morgen@protonmail.com>
2023-03-06 12:09:57 +0100

    more tests won't hurt

--- tests/testthat/test_rank_utils.R
+++ tests/testthat/test_rank_utils.R
@@ -1,3 +1,83 @@
+### compare ###
+
+test_that("compare works for sorted input", {
+  x <- c(1,3,4,4,4,7,7,10)
+  expected_compare_om0.4 <- matrix(c(
+    0.4, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
+    1.0, 0.4, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
+    1.0, 1.0, 0.4, 0.4, 0.4, 0.0, 0.0, 0.0,
+    1.0, 1.0, 0.4, 0.4, 0.4, 0.0, 0.0, 0.0,
+    1.0, 1.0, 0.4, 0.4, 0.4, 0.0, 0.0, 0.0,
+    1.0, 1.0, 1.0, 1.0, 1.0, 0.4, 0.4, 0.0,
+    1.0, 1.0, 1.0, 1.0, 1.0, 0.4, 0.4, 0.0,
+    1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.4
+  ), byrow = TRUE, ncol = 8)
+  expected_compare_om0 <- expected_compare_om0.4
+  expected_compare_om0[expected_compare_om0 == 0.4] <- 0
+  expected_compare_om1 <- expected_compare_om0.4
+  expected_compare_om1[expected_compare_om1 == 0.4] <- 1
+  
+  expect_equal(compare(x, omega = 0, increasing = TRUE, na.rm = FALSE),
+               expected_compare_om0)
+  expect_equal(compare(x, omega = 0.4, increasing = TRUE, na.rm = FALSE),
+               expected_compare_om0.4)
+  expect_equal(compare(x, omega = 1, increasing = TRUE, na.rm = FALSE),
+               expected_compare_om1)
+})
+
+test_that("compare works for unsorted input", {
+  x <- c(4, 3, 4, 4, 7, 7, 10, 1)
+  expected_compare_om0.4 <- matrix(c(
+    0.4, 1.0, 0.4, 0.4, 0.0, 0.0, 0.0, 1.0,
+    0.0, 0.4, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+    0.4, 1.0, 0.4, 0.4, 0.0, 0.0, 0.0, 1.0,
+    0.4, 1.0, 0.4, 0.4, 0.0, 0.0, 0.0, 1.0,
+    1.0, 1.0, 1.0, 1.0, 0.4, 0.4, 0.0, 1.0,
+    1.0, 1.0, 1.0, 1.0, 0.4, 0.4, 0.0, 1.0,
+    1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.4, 1.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.4
+  ), byrow = TRUE, ncol = 8)
+  expected_compare_om0 <- expected_compare_om0.4
+  expected_compare_om0[expected_compare_om0 == 0.4] <- 0
+  expected_compare_om1 <- expected_compare_om0.4
+  expected_compare_om1[expected_compare_om1 == 0.4] <- 1
+  
+  expect_equal(compare(x, omega = 0, increasing = TRUE, na.rm = FALSE),
+               expected_compare_om0)
+  expect_equal(compare(x, omega = 0.4, increasing = TRUE, na.rm = FALSE),
+               expected_compare_om0.4)
+  expect_equal(compare(x, omega = 1, increasing = TRUE, na.rm = FALSE),
+               expected_compare_om1)
+})
+
+test_that("compare handles NAs", {
+  x <- c(4, 3, 4, NA, 7, 7, NA, 1)
+  expected_compare_na.rmT <- matrix(c(
+    0.4, 1.0, 0.4, 0.0, 0.0, 1.0,
+    0.0, 0.4, 0.0, 0.0, 0.0, 1.0,
+    0.4, 1.0, 0.4, 0.0, 0.0, 1.0,
+    1.0, 1.0, 1.0, 0.4, 0.4, 1.0,
+    1.0, 1.0, 1.0, 0.4, 0.4, 1.0,
+    0.0, 0.0, 0.0, 0.0, 0.0, 0.4
+  ), byrow = TRUE, ncol = 6)
+  expected_compare_na.rmF <- matrix(c(
+    0.4, 1.0, 0.4, NA, 0.0, 0.0, NA, 1.0,
+    0.0, 0.4, 0.0, NA, 0.0, 0.0, NA, 1.0,
+    0.4, 1.0, 0.4, NA, 0.0, 0.0, NA, 1.0,
+     NA,  NA,  NA, NA,  NA,  NA, NA,  NA, 
+    1.0, 1.0, 1.0, NA, 0.4, 0.4, NA, 1.0,
+    1.0, 1.0, 1.0, NA, 0.4, 0.4, NA, 1.0,
+     NA,  NA,  NA, NA,  NA,  NA, NA,  NA,
+    0.0, 0.0, 0.0, NA, 0.0, 0.0, NA, 0.4
+  ), byrow = TRUE, ncol = 8)
+  expect_equal(compare(x, omega = 0.4, increasing = TRUE, na.rm = TRUE),
+               expected_compare_na.rmT)
+  expect_equal(compare(x, omega = 0.4, increasing = TRUE, na.rm = FALSE),
+               expected_compare_na.rmF)
+})
+
+### irank ###
+
 test_that("irank works for sorted input", {
   x <- c(1,3,4,4,4,7,7,10)
   expected_irank_om0 <- c(8,7,4,4,4,2,2,1)
@@ -26,15 +106,3 @@ test_that("irank works for unsorted input", {
                expected_irank_om0.5)
 })
 
-test_that("irank with na.rm handles NAs", {
-  x <- c(1,3,4,4,4,7,7,10, NA, NA, NA)
-  expected_irank_om0 <- c(8,7,4,4,4,2,2,1)
-  expected_irank_om1 <- c(8,7,6,6,6,3,3,1)
-  expected_irank_om0.5 <- c(8,7,5,5,5,2.5,2.5,1)
-  expect_equal(irank(x, omega = 0, na.rm=TRUE),
-               expected_irank_om0)
-  expect_equal(irank(x, omega = 1, na.rm=TRUE),
-               expected_irank_om1)
-  expect_equal(irank(x, omega = 0.5, na.rm=TRUE),
-               expected_irank_om0.5)
-})
