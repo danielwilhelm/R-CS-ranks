c2cff917609bd3edda88c1a11090a96b61a61040
MrDomani <pawel.morgen@protonmail.com>
2023-03-07 15:45:01 +0100

    rrreg -> simple_lmranks

--- R/rrreg.R
+++ R/rrreg.R
@@ -1,16 +1,38 @@
-rrreg <- function(Y, X, W=NULL, omega=0, increasing=FALSE, na.rm=FALSE) {
+#' Linear model for ranks
+#' 
+#' Fit a linear model with a single rank response and
+#' a single rank covariate (and possibly other usual covariates).
+#'
+#' @param Y Numeric vector. Based on it, ranks for response will be constructed
+#' and used as final response in the linear model.
+#' @param X Numeric vector. Based on it, ranks for this covariate will be constructed
+#' and used as final covariate in the linear model.
+#' @param W Matrix. Other covariates to include in the model. Should not include an intercept column.
+#' @param omega numeric; numeric value in [0,1], each corresponding to a different definition of the rank; default is \code{0}. See \code{\link{frank}} for details.
+#' @param increasing logical; if \code{TRUE}, then large elements in \code{X} and \code{Y} receive a large rank. Otherwise, large elements receive small ranks. 
+#' @param na.rm logical; if \code{TRUE}, then \code{NA}'s are removed from \code{X} and \code{Y} (if any). 
+
+#' @return A list with two items: 
+#' \itemize{
+#' \item{\code{rhohat} - the linear coefficient for rank of X.}
+#' \item{\code{se} - Estimated standard error of the regression.}
+#' }
+#' @note 
+#' TODO: make it more similar to lm.
+#' @export
+simple_lmranks <- function(Y, X, W=NULL, omega=0, increasing=FALSE, na.rm=FALSE) {
   I_Y <- compare(Y, omega=omega, increasing=increasing, na.rm=na.rm)
   I_X <- compare(X, omega=omega, increasing=increasing, na.rm=na.rm)
-  RY <- rowMeans(I_Y)
-  RX <- rowMeans(I_X)
+  RY <- (rowSums(I_Y) + 1 - omega) / length(Y)
+  RX <- (rowSums(I_X) + 1 - omega) / length(X)
   
 	if (any(is.null(W))) {
-		W <- rep(1,length(Y))
+		W <- matrix(rep(1,length(Y)), ncol = 1)
 	} else {
 	  W <- cbind(rep(1,length(Y)),W)
 	}
   
-  RX_by_W <- lm(RX ~ W-1)
+  RX_by_Ws <- lm(RX ~ W-1)
   Wgammahat <- predict(RX_by_Ws)
   nuhat <- resid(RX_by_Ws)
   gammahat <- coef(RX_by_Ws)
@@ -22,14 +44,18 @@ rrreg <- function(Y, X, W=NULL, omega=0, increasing=FALSE, na.rm=FALSE) {
   
   h1 <- epsilonhat * nuhat
   
-  h2 <- colMeans((I_Y - rhohat * I_X - c(W %*% betahat)) * nuhat)
+  # vectorized `-` and `*` goes over rows
+  h2 <- colMeans((t(I_Y - rhohat * I_X) - c(W %*% betahat)) * nuhat)
   
-  h3 <- colMeans(epsilonhat * (I_X - Wgammahat))
+  h3 <- colMeans(epsilonhat * (t(I_X) - Wgammahat))
   
   sigmahat <- mean((h1+h2+h3)^2) / var(nuhat)^2
   se <- sqrt(sigmahat / length(Y))
+  names(rhohat) <- NULL
 	return(list(rhohat=rhohat, se=se))
 }
+
+rrreg <- simple_lmranks
 
 rrregSE <- function(Y, X, W=NULL, omega, increasing, na.rm){
   rrreg_outcome <- rrreg(Y, X, W=W, omega=omega, increasing=increasing, na.rm=na.rm)
