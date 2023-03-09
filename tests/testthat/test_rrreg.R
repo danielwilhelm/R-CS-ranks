3c8c71c9e0bb405cd4de423851d76d3e281eba0f
MrDomani <pawel.morgen@protonmail.com>
2023-03-09 11:19:45 +0100

    better tests

--- tests/testthat/test_rrreg.R
+++ tests/testthat/test_rrreg.R
@@ -19,11 +19,11 @@ test_that("simple_lmranks works for no correlation",{
 })
 
 test_that("simple_lmranks works for NULL W", {
-  Y <- c(2,0,0,3,3)
+  Y <- c(3,1,2,4,5)
   omega <- 0.5
-  y_frank <- c(0.6, 0.9, 0.9, 0.3,0.3)
+  y_frank <- c(0.6, 1.0, 0.8, 0.4, 0.2)
   X <- 1:5
-  x_frank <- (5:1) / 5
+  x_frank <- c(1.0, 0.8, 0.6, 0.4, 0.2)
   m <- lm(y_frank ~ x_frank)
   expected_rho <- coef(m)[2]
   names(expected_rho) <- NULL
@@ -42,9 +42,41 @@ test_that("simple_lmranks works for NULL W", {
       (y_frank[j] - expected_rho * x_frank[j] - betahat) * 
          (omega * (X[i] <= X[j]) + (1-omega)*(X[i] < X[j]) - gammahat)
     })))
-  expected_se <- sqrt(mean((h1 + h2 + h3)^2) / sigma_nu^2)
+  expected_se <- sqrt(mean((h1 + h2 + h3)^2) / sigma_nu^2 / 5)
   
   actual <- simple_lmranks(Y, X, omega = omega)
   expect_equal(actual$rhohat, expected_rho)
-  expect_equal(actual$se * sqrt(5), expected_se)
+  expect_equal(actual$se, expected_se)
+})
+
+test_that("simple_lmranks works for non-NULL W, fully correlated with RY", {
+  Y <- c(3,1,2,4,5)
+  y_frank <- c(0.6, 1.0, 0.8, 0.4, 0.2)
+  X <- 1:5
+  omega <- 0.5
+  x_frank <- c(1.0, 0.8, 0.6, 0.4, 0.2)
+  W <- y_frank * 0.1 + 5
+  
+  m <- lm(y_frank ~ x_frank + W)
+  
+  gammahat <- 0.6 # mean of frank of x
+  betahat <- coef(m)[3]
+  beta_0 <- coef(m)[1]
+  X_W <- lm(x_frank ~ W)
+  gammahat <- coef(X_W)[2]
+  gamma_0 <- coef(X_W)[1]
+  sigma_nu <- var(resid(X_W))
+  
+  expected_rho <- 0
+  h2 <- sapply(1:5, function(i)
+    mean(sapply(1:5, function(j){
+      (omega * (Y[i] <= Y[j]) + (1-omega)*(Y[i] < Y[j]) - W[j] * betahat - beta_0) * 
+        resid(X_W)[j]
+    })))
+  
+  expected_se <- sqrt(mean(h2^2) / sigma_nu^2 / 5)
+  
+  actual <- simple_lmranks(Y, X, W, omega=omega)
+  expect_equal(actual$rhohat, expected_rho)
+  expect_equal(actual$se, expected_se)
 })
