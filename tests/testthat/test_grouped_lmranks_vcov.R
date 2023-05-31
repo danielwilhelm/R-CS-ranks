1accf43f517597391a3acb5160455c43a114605f
MrDomani <pawel.morgen@protonmail.com>
2023-05-31 13:37:47 +0200

    cache test results

--- tests/testthat/test_grouped_lmranks_vcov.R
+++ tests/testthat/test_grouped_lmranks_vcov.R
@@ -1,91 +1,15 @@
-test_that("vcov produces correct asymptotic variance estimate of rank-rank slope", {
-  set.seed(100)
+test_that("vcov produces correct asymptotic variance estimate of rank-rank slope with no covariates", {
+  load(test_path("testdata", "grouped_lmranks_cov_sigmahat_covariates_FALSE.rda"))
+  res <- grouped_lmranks(r(Y) ~ r(X), data=data.frame(Y=Y,X=X), grouping_factor=G, omega=1)
+  cov_mats <- calculate_grouped_lmranks_covariances(res)
+  sigma2hat.grouped_lmranks <- c(cov_mats[[1]][2,2]*n, cov_mats[[2]][2,2]*n)
+  expect_equal(sigma2hat, sigma2hat.grouped_lmranks, tolerance=1e-5)
+})
 
-  for (covariates in c(TRUE,FALSE)) {
-    
-    # draw data
-    n <- 1000
-    G <- c(rep(1,n/2),rep(2,n/2))
-    beta <- G
-    if (covariates) {
-      X <- rnorm(n)
-      W <- matrix(rnorm(n*2), n, 2)
-      Y <- beta/2 + beta*X + rowSums(W) + rnorm(n,0,0.5)  
-      W <- cbind(1,W)  
-    } else {
-      X <- rnorm(n)
-      Y <- beta/2 + beta*X + rnorm(n,0,0.5) 
-      W <- matrix(1,n,1)
-    }
-
-    # compute ranks
-    RY <- frank(Y, increasing=TRUE, omega=1)
-    RX <- frank(X, increasing=TRUE, omega=1)
-    RY1 <- RY[1:(n/2)]; RX1 <- RX[1:(n/2)]; W1 <- W[1:(n/2),]
-    RY2 <- RY[(n/2+1):n]; RX2 <- RX[(n/2+1):n]; W2 <- W[(n/2+1):n,]
-
-
-    # ------- compute asymptotic variance "by hand"
-      
-      Ifn <- function(u, v) return( u<=v )
-
-      # first stage
-      res1 <- lm(RX1 ~ W1-1)
-      Wgammahat1 <- predict(res1)
-      nuhat1 <- resid(res1)
-      gammahat1 <- coef(res1)
-      res2 <- lm(RX2 ~ W2-1)
-      Wgammahat2 <- predict(res2)
-      nuhat2 <- resid(res2)
-      gammahat2 <- coef(res2)
-      nuhat <- c(nuhat1,nuhat2)
-      Wgammahat <- c(Wgammahat1,Wgammahat2)
-
-      # outcome equation
-      res1 <- lm(RY1~RX1+W1-1)
-      rhohat1 <- coef(res1)[1]
-      betahat1 <- coef(res1)[-1]
-      epsilonhat1 <- resid(res1)
-      res2 <- lm(RY2~RX2+W2-1)
-      rhohat2 <- coef(res2)[1]
-      betahat2 <- coef(res2)[-1]
-      epsilonhat2 <- resid(res2)
-      epsilonhat <- c(epsilonhat1,epsilonhat2)
-
-      # construct H1
-      H11 <- c(epsilonhat1 * nuhat1, rep(0,n/2))
-      H12 <- c(rep(0,n/2), epsilonhat2 * nuhat2)
-
-      # construct H2
-      H21fn <- function(xy) mean((G==1) * (Ifn(xy[2],Y)-rhohat1*Ifn(xy[1],X)-c(W%*%betahat1)) * nuhat)
-      H21 <- apply(cbind(X,Y), 1, H21fn)
-      H22fn <- function(xy) mean((G==2) * (Ifn(xy[2],Y)-rhohat2*Ifn(xy[1],X)-c(W%*%betahat2)) * nuhat)
-      H22 <- apply(cbind(X,Y), 1, H22fn)
-
-      # construct H3
-      H31fn <- function(x) mean((G==1) * epsilonhat * (Ifn(x,X)-Wgammahat))
-      H31 <- sapply(X, H31fn)
-      H32fn <- function(x) mean((G==2) * epsilonhat * (Ifn(x,X)-Wgammahat))
-      H32 <- sapply(X, H32fn)
-
-      # compute asymptotic variance
-      sigma2hat <- c(mean((H11+H21+H31)^2) / mean((G==1)*nuhat^2)^2,
-                      mean((H12+H22+H32)^2) / mean((G==2)*nuhat^2)^2)
-
-
-    # ------- compute asymptotic variance using grouped_lmranks
-
-      if (covariates) { 
-        res <- grouped_lmranks(r(Y) ~ r(X) + W - 1, data=data.frame(Y=Y,X=X), grouping_factor=G, omega=1)
-        sigma2hat.grouped_lmranks <- c(calculate_grouped_lmranks_covariances(res)[[1]][2,2]*n, calculate_grouped_lmranks_covariances(res)[[2]][2,2]*n)
-      } else {
-        res <- grouped_lmranks(r(Y) ~ r(X), data=data.frame(Y=Y,X=X), grouping_factor=G, omega=1)
-        sigma2hat.grouped_lmranks <- c(calculate_grouped_lmranks_covariances(res)[[1]][2,2]*n, calculate_grouped_lmranks_covariances(res)[[2]][2,2]*n)
-      }
-
-      
-    # ------- test equality
-    
-      expect_equal(sigma2hat, sigma2hat.lmranks)
-  }
+test_that("vcov produces correct asymptotic variance estimate of rank-rank slope with no covariates", {
+  load(test_path("testdata", "grouped_lmranks_cov_sigmahat_covariates_TRUE.rda"))
+  res <- grouped_lmranks(r(Y) ~ r(X) + W - 1, data=data.frame(Y=Y,X=X), grouping_factor=G, omega=1)
+  cov_mats <- calculate_grouped_lmranks_covariances(res)
+  sigma2hat.grouped_lmranks <- c(cov_mats[[1]][1,1]*n, cov_mats[[2]][1,1]*n)
+  expect_equal(sigma2hat, sigma2hat.grouped_lmranks, tolerance=1e-5)
 })
