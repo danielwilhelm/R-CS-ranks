38816a0d3bcf48d25d83027697fca72d0f79b9b4
MrDomani <pawel.morgen@protonmail.com>
2023-05-31 13:37:23 +0200

    bugfixes

--- R/grouped_lmranks_summary.R
+++ R/grouped_lmranks_summary.R
@@ -20,16 +20,16 @@ calculate_grouped_lmranks_covariances <- function(object, complete = TRUE, ...){
     l <- get_and_separate_regressors(group_model)
     group_RX <- l$RX; rank_column_index <- l$rank_column_index
     group_RY <- model.response(model.frame(group_model))
-    if(object$ranked_response){
+    if(group_model$ranked_response){
       n_lequal_lesser_Y <- count_lequal_lesser(global_RY, group_RY, 
                                                return_inverse_ranking=TRUE)
     } else {
       n_lequal_lesser_Y <- NULL
     }
-    if(length(object$rank_terms_indices) == 1){
+    if(length(group_model$rank_terms_indices) == 1){
       n_lequal_lesser_X <- count_lequal_lesser(global_RX, group_RX,
                                                return_inverse_ranking=TRUE)
-    } else if(length(object$rank_terms_indices) == 0) {
+    } else if(length(group_model$rank_terms_indices) == 0) {
       n_lequal_lesser_X <- NULL
     } else {
       cli::cli_abort("Not implemented")
@@ -42,8 +42,9 @@ calculate_grouped_lmranks_covariances <- function(object, complete = TRUE, ...){
       }
       proj_model <- get_projection_model(group_model, i)
       h_1 <- calculate_h_1(group_model, proj_model, group_indicator)
-      h_2 <- calculate_h_2(group_model, proj_model, I_X=I_X, I_Y=I_Y)
-      h_3 <- calculate_h_3(group_model, proj_model, I_X=I_X)
+      h_2 <- calculate_h_2(group_model, proj_model, n_lequal_lesser_X=n_lequal_lesser_X, 
+                           n_lequal_lesser_Y=n_lequal_lesser_Y)
+      h_3 <- calculate_h_3(group_model, proj_model, n_lequal_lesser_X=n_lequal_lesser_X)
       var_estimate <- sum(resid(proj_model)^2) / length(global_RX)
       (h_1 + h_2 + h_3) / var_estimate
     })
@@ -69,16 +70,19 @@ calculate_h_1 <- function(original_model, proj_model, group_indicator){
 calculate_h_2 <- function(original_model, proj_model, n_lequal_lesser_X, n_lequal_lesser_Y){
  nuhat <- resid(proj_model)
  # Correct for mean in next function. Expectation over whole dataset, not just cluster
- nuhat <- nuhat * stats::nobs(original_model) / nrow(n_lequal_lesser_X) 
+ nuhat <- nuhat * stats::nobs(original_model) / get_global_nobs(n_lequal_lesser_X, NULL) 
  return(calculate_weighted_ineq_resid_means(
    original_model, weights=nuhat, n_lequal_lesser_X=n_lequal_lesser_X, n_lequal_lesser_Y=n_lequal_lesser_Y
  ))
 }
 
-calculate_h_3 <- function(original_model, proj_model, I_X){
+calculate_h_3 <- function(original_model, proj_model, n_lequal_lesser_X){
+  if(is.null(n_lequal_lesser_X)){
+    return(0)
+  }
   epsilonhat <- resid(original_model)
   # Correct for mean in next function. Expectation over whole dataset, not just cluster
-  epsilonhat <- epsilonhat * stats::nobs(original_model) / nrow(n_lequal_lesser_X) 
+  epsilonhat <- epsilonhat * stats::nobs(original_model) / get_global_nobs(n_lequal_lesser_X, NULL) 
   if(proj_model$ranked_response){
     return(calculate_weighted_ineq_resid_means(
       proj_model, weights=epsilonhat, n_lequal_lesser_Y=n_lequal_lesser_X))
