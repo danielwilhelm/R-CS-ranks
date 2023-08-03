5feef25a613585b385d21cf181c1457452067d94
MrDomani <pawel.morgen@protonmail.com>
2023-08-03 16:42:19 +0200

    removed duplicated tests

--- tests/testthat/test_lmranks.R
+++ tests/testthat/test_lmranks.R
@@ -91,50 +91,10 @@ test_that("lmranks correctly estimates rank correlation", {
   }  
 })
 
-test_that("lmranks correctly estimates OLS slope and intercept within each group", {
-  set.seed(100)
-  
-  for (dist in c("discr", "cont")) {
-    
-    if (dist == "discr") {
-      X <- rbinom(200, 5, 0.5)
-      Y <- c(rep(1,100),rep(2,100))*X + rbinom(200, 2, 0.5)
-    } else {
-      X <- rnorm(200)
-      Y <- c(rep(1,100),rep(2,100))*X + rnorm(200)
-    }
-    G <- factor(c(rep(1,100),rep(2,100)))
-    dat <- data.frame(Y=Y, X=X)
-    
-    for (omega in c(0, 0.5, 1)) {
-      RY <- frank(Y, omega=omega, increasing=TRUE)
-      RX <- frank(X, omega=omega, increasing=TRUE)
-      RY1 <- RY[1:100]; RX1 <- RX[1:100]
-      RY2 <- RY[101:200]; RX2 <- RX[101:200]
-      coeffs.lm <- as.numeric(cbind(coef(lm(RY1~RX1)), coef(lm(RY2~RX2))))
-      
-      res <- lmranks(r(Y) ~ G+r(X):G - 1, data=dat, omega=omega)
-      coeffs.grouped_lmranks <- as.numeric(coef(res))
-      expect_equal(coeffs.lm[c(1,3,2,4)], coeffs.grouped_lmranks)     
-    }
-    
-  }
-  
-})
-
 test_that("lmranks raises error if NA is encountered in data", {
   data(mtcars)
   mtcars[5, "disp"] <- NA
   expect_error(lmranks(r(mpg) ~ r(hp) + disp, data=mtcars), "missing values")
-})
-
-test_that("lmranks raises error if grouping variable is not a factor", {
-  data(mtcars)
-  mtcars$vs <- mtcars$vs + 1
-  expect_error(lmranks(r(mpg) ~ (r(hp) + disp):vs, data=mtcars), "factor")
-  
-  mtcars$vs <- factor(mtcars$vs)
-  expect_silent(lmranks(r(mpg) ~ (r(hp) + disp):vs, data=mtcars))
 })
 
 test_that("process_lmranks_formula catches illegal formulas", {
