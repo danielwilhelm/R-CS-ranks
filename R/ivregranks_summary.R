348fd3198466ffbadfae6587f36b26fbae2f1d92
muskuloes <vic.tuekam@gmail.com>
2025-08-10 18:08:07 +0200

    start summary implementation

---
+++ R/ivregranks_summary.R
@@ -0,0 +1,14 @@
+#' Title
+#'
+#' @param object
+#' @param vcov.
+#' @param df
+#' @param diagnostics
+#' @param ...
+#'
+#' @return
+#' @export
+summary.ivregranks <- function(object, vcov. = NULL, df = NULL,
+                               diagnostics = NULL, ...) {
+
+}
