f96a9bcba69b467fb89c0258b675a64c201c7e65
MrDomani <pawel.morgen@pm.me>
2026-07-13 16:01:17 +0200

    test: removed tests against private methods of r(), focusing on testing external APIs

--- tests/testthat/test_lmranks.R
+++ tests/testthat/test_lmranks.R
@@ -282,87 +282,24 @@ test_that("create_env_to_interpret_r_mark has a correct parent env", {
   expect_reference(parent.env(created_env), expected_parent_env)
 })
 
-test_that("create_env_to_interpret_r_mark has correct contents", {
-  expected_env_contents <- list(
-    .r_predict = FALSE,
-    .r_cache = list(),
-    r = function(x, increasing = TRUE) {}
-  )
-
-  created_env <- create_env_to_interpret_r_mark(omega = 0.4)
-  actual_env_contents <- as.list(created_env, all.names = TRUE)
-  expect_equal(
-    names(actual_env_contents),
-    names(expected_env_contents)
-  )
-  expect_equal(
-    actual_env_contents[c(".r_predict", ".r_cache")],
-    expected_env_contents[c(".r_predict", ".r_cache")]
-  )
-  expect_true(is.function(actual_env_contents[["r"]]))
-})
-
-test_that("create_env_to_interpret_r_mark's r function behaves correctly in fitting", {
-  x_1 <- c(4, 4, 4, 3, 1, 10, 7, 7)
-  expected_r_output <- c(0.475, 0.475, 0.475, 0.250, 0.125, 1.000, 0.800, 0.800)
-  wrapper <- function(omega) {
-    create_env_to_interpret_r_mark(omega = omega)
-  }
-  created_env <- wrapper(0.4)
-  actual_r_output <- eval(quote(r(x_1)), created_env)
-  expect_equal(actual_r_output, expected_r_output)
-
-  expected_r_output_om0 <- c(0.375, 0.375, 0.375, 0.250, 0.125, 1.000, 0.750, 0.750)
-  created_env <- wrapper(0)
-  actual_r_output <- eval(quote(r(x_1)), created_env)
-  expect_equal(actual_r_output, expected_r_output_om0)
-
-  expected_r_output_om1 <- c(0.625, 0.625, 0.625, 0.25, 0.125, 1.0, 0.875, 0.875)
-  created_env <- wrapper(1)
-  actual_r_output <- eval(quote(r(x_1)), created_env)
-  expect_equal(actual_r_output, expected_r_output_om1)
-})
-
-test_that("create_env_to_interpret_r_mark's r function behaves correctly in prediction", {
-  x_1 <- c(4, 4, 4, 3, 1, 10, 7, 7)
-  x_pred <- c(0, 1, 2, 3, 4, 5, 7, 8, 10, 11)
-  expected_r_output <- ecdf(x_1)(x_pred)
-  wrapper <- function(omega) {
-    create_env_to_interpret_r_mark(omega = omega)
-  }
-  created_env <- wrapper(1.0)
-
-  assign(".r_cache", list(x_pred = x_1), envir = created_env)
-  assign(".r_predict", TRUE, envir = created_env)
-
-  actual_r_output <- eval(quote(r(x_pred)), created_env)
-  expect_equal(actual_r_output, expected_r_output)
-})
-
-test_that("create_env_to_interpret_r_mark's r function handles NA in prediction", {
-  x_1 <- c(4, 4, 4, 3, 1, 10, 7, 7)
-  x_pred <- c(0, 1, NA, 3, 4, 5, 7, NA, 10, 11)
-  expected_r_output <- ecdf(x_1)(x_pred)
-
-  wrapper <- function(omega, na.rm) {
-    create_env_to_interpret_r_mark(omega = omega)
-  }
-  created_env <- wrapper(1.0)
-
-  assign(".r_cache", list(x_pred = x_1), envir = created_env)
-  assign(".r_predict", TRUE, envir = created_env)
-
-  actual_r_output <- eval(quote(r(x_pred)), created_env)
-  expect_equal(actual_r_output, expected_r_output)
-})
-
-test_that("omega argument is passed further correctly", {
+test_that("r() works and omega argument is passed further correctly", {
   Y <- c(4, 4, 4, 3, 1, 10, 7, 7)
-  y_frank <- c(0.475, 0.475, 0.475, 0.250, 0.125, 1.000, 0.800, 0.800)
   X <- 1:8
   W <- matrix(c(1, 4, 3, 2, 5, 8, 7, 6), ncol = 1)
 
+
+  expected_y <- c(0.475, 0.475, 0.475, 0.250, 0.125, 1.000, 0.800, 0.800)
   rank_m <- lmranks(r(Y) ~ r(X) + W, omega = 0.4, y = TRUE)
   names(rank_m$y) <- NULL
-  expect_equal(rank_m$y, y_frank)
+  expect_equal(rank_m$y, expected_y)
+
+  expected_y <- c(0.375, 0.375, 0.375, 0.250, 0.125, 1.000, 0.750, 0.750)
+  rank_m <- lmranks(r(Y) ~ r(X) + W, omega = 0, y = TRUE)
+  names(rank_m$y) <- NULL
+  expect_equal(rank_m$y, expected_y)
+
+  expected_y <- c(0.625, 0.625, 0.625, 0.25, 0.125, 1.0, 0.875, 0.875)
+  rank_m <- lmranks(r(Y) ~ r(X) + W, omega = 1, y = TRUE)
+  names(rank_m$y) <- NULL
+  expect_equal(rank_m$y, expected_y)
 })
